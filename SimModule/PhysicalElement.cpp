652717704fcb11df9d913eb44e1e095bf84341fc
Doz <doraneplus@gmail.com>
2015-09-20 16:01:54 +0100

    Update PhysicalElement.cpp

--- SimModule/PhysicalElement.cpp
+++ SimModule/PhysicalElement.cpp
@@ -1,115 +1,134 @@
 #include "PhysicalElement.h"
-
-#include "Geo.h"
 #include <iostream>
+
+//-------------------------- Instanciated Vector ------------------------------------
+
+//Constructors and destructor
+InstanciatedVect::InstanciatedVect()
+{}
+
+InstanciatedVect::InstanciatedVect(Vect V)
+{
+	Point P;
+	P.nullify();
+	application = &P; 
+	direction = V;
+}
+
+InstanciatedVect::InstanciatedVect(Point P, Vect V)
+{
+	application = &P;
+	direction = V;
+}
+
+InstanciatedVect::~InstanciatedVect()
+{}
+
+//Accessors
+Vect InstanciatedVect::getDirection() { return direction; } //**
+
+Point *InstanciatedVect::getApplicationPoint() //returns a pointer to the application point**
+{
+	return application;
+}
+
+//Display
+void InstanciatedVect::show()
+{
+	std::cout << "Direction:" << '\n';
+	direction.show();
+
+	std::cout << "Application point:" << '\n';
+	application->show();
+}
+
+//Modifier
+void InstanciatedVect::setDirection(Vect V)     //**
+{ direction.set(V); }
+
+void InstanciatedVect::setDirection(double x, double y, double z)
+{ direction.set(x,y,z); }
+
+
+void InstanciatedVect::setApplication(Point *P)
+{ application = P; }
+
+void InstanciatedVect::nullify()
+{
+	direction.nullify();
+	application->nullify();
+}
+
+
+
 
 //-------------------------- Forces ------------------------------------
 
 	//Constructors and destructor
 	Force::Force()
 	{}
+
+	Force::Force(Point P, Vect V) : InstanciatedVect(P,V)
+	{}
+
 	Force::~Force()
 	{}
-	
-	//Accessors
-	Vector Force::getDirection()
-	{ return direction;}
-	
-    Point *Force::getApplicationPoint()
-	{ return application;}
-	
+		
 	//Display
 	void Force::show()
 	{
-		std::cout<<"Direction:"<<'\n';
-		direction.show();
-		
-		std::cout<<"Application point:"<<'\n';
-        application->show();
+		std::cout << "   ---- Force ----   " << '\n';
+		InstanciatedVect::show();
 	}
 	
-	//Modifier
-	void Force::setDirection (Vector V)
-	{direction = V;}
-
-    void Force::setApplication(Point *P)
-    {application = P;}
-
-	void Force::nullify()
-	{direction.nullify();}
-
-    Force Force::operator +(Force B ) //supposes same application point
-    {Force F;
-        F.setDirection(direction+B.direction);
-        F.application= application;
-    return F;
+	
+    Force Force::operator +(Force B ) //returns the sum of 2 forces applying to their middlepoint
+    {
+	 Force F;
+        F.setDirection(getDirection() + B.getDirection());
+		F.getApplicationPoint()->set(*getApplicationPoint() + *(B.getApplicationPoint()));
+     return F;
     }
 
     Force Force::operator -()
-    {Force F; Vector V;
-        double x=this->getDirection().getx();
-        double y=this->getDirection().gety();
-        double z=this->getDirection().getz();
+    {
+	 Force F; Vect V;
+        double x=this->getDirection().getX();
+        double y=this->getDirection().getY();
+        double z=this->getDirection().getZ();
 
         x=-x;y=-y;z=-z;
 
-        V.setVector(x,y,z);
+        V.set(x,y,z);
         F.setDirection(V);
         F.setApplication( getApplicationPoint());
-    return F;
+     return F;
     }
 
-//	Vector Force::operator *( Vector A, Vector B )  //produit vectoriel
-//	Vector Force::operator *( double a, Vector B )  //produit scalaire
+
 
 //-------------------------- Acceleration ------------------------------------
 //Constructors and destructor
 	Acceleration::Acceleration()
 	{}
 	
-	Acceleration::Acceleration(Force F, double mass) // valid for point element
-	{
-        application = F.getApplicationPoint();
-		direction = F.getDirection()*(1/mass);
+	Acceleration::Acceleration(Force Resultant, double mass) 
+	{// returns the linear acceleration of the center of gravity of an object given its mass and resultant
+	
+        application = Resultant.getApplicationPoint();
+		direction = Resultant.getDirection() / mass;
 	}
 	
-
 	Acceleration::~Acceleration()
 	{}
-	
-	//Accessors
-	Vector Acceleration::getDirection()
-	{ return direction;}
-	
-    Point *Acceleration::getApplicationPoint()
-	{return application;}
 	
 	//Display
 	void Acceleration::show()
 	{
-		std::cout<<"Direction:"<<'\n';
-		direction.show();
-		
-		std::cout<<"Application point:"<<'\n';
-        application->show();
+		std::cout << "   ---- Acceleration ----   " << '\n';
+		InstanciatedVect::show();
 	}
 	
-	//Modifier
-	void Acceleration::setDirection (Vector V)
-	{direction = V;}
-	
-    void Acceleration::setApplication(Point *P)
-    {application=P;}
-	
-	void Acceleration::nullify()
-	{direction.nullify();}
-	
-//	void Acceleration::setApplication(double x, double y, double z)
-	
-//	Vector Acceleration::operator +( Vector A, Vector B )
-//	Vector Acceleration::operator *( Vector A, Vector B )  //produit vectoriel
-//	Vector Acceleration::operator *( double a, Vector B )  //produit scalaire
-
 
 
 //-------------------------- Velocity ------------------------------------
@@ -120,43 +139,9 @@
 	Velocity::~Velocity()
 	{}
 	
-	//Accessors
-	Vector Velocity::getDirection()
-	{ return direction;}
-	
-    Point *Velocity::getApplicationPoint()
-	{return application;}
-	
 	//Display
 	void Velocity::show()
 	{
-		std::cout<<"Direction:"<<'\n';
-		direction.show();
-		
-		std::cout<<"Application point:"<<'\n';
-        application->show();
+		std::cout << "   ---- Velocity ----   " << '\n';
+		InstanciatedVect::show();
 	}
-	
-	//Modifier
-	void Velocity::setDirection (Vector V)
-	{direction = V;}
-	
-	void Velocity::setDirection (double x, double y, double z)
-	{
-		direction.setVector(x,y,z);
-	}
-	
-    void Velocity::setApplication(Point *P)
-    {application=P;}
-	
-	void Velocity::nullify()
-	{direction.nullify();}
-	
-	
-//	void Velocity::setApplication(double x, double y, double z)
-//	Vector Velocity::operator +( Vector A, Vector B )
-//	Vector Velocity::operator *( Vector A, Vector B )  //produit vectoriel
-//	Vector Velocity::operator *( double a, Vector B )  //produit scalaire
-
-
-
