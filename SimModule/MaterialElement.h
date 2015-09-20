52c0b6ca2da232323be0e6d14617b5c48c3f8c6b
Doz <doraneplus@gmail.com>
2015-09-20 16:03:08 +0100

    Update MaterialElement.h

--- SimModule/MaterialElement.h
+++ SimModule/MaterialElement.h
@@ -11,21 +11,20 @@ class MaterialElement
 {
 private:
 	
-	double mass;   	// in kg
-	double charge; 	// in Coulomb
-	Point position; // m 
-	Force netForce; // Newton
-	Velocity V;		// m/s
+	double Mass;   	// in kg
+	double Charge; 	// in Coulomb
+	Point Position; // m 
+	Force NetForce; // Newton
+    Velocity V;		// m/s
 	
-	virtual void updateSpeedandPosition(Acceleration a, double dt); 
-
+	virtual void updateSpeedandPosition(Acceleration a, double dt);
 
 public:
 
 
 	//Constructors & destructor
 	MaterialElement();	
-    MaterialElement(Point P, double masse =0, double charge_=0);
+    MaterialElement(Point p, double masse =0, double charge_=0);
 	~MaterialElement();		
 
 	//Accessors
@@ -36,15 +35,13 @@ public:
 	virtual void consoleShow();
 	
 	//Modifier
-    void setPosition(Point &P);
+    void setPosition(Point &p);
 	void setPosition(double x, double y, double z);
 	void setMass(double m);
 	
 	//Simulation tool
 	virtual void update(double dt); //computes and updates the state of the element at t+dt
 	virtual Acceleration getAcceleration();
-
-	
 };
 
 
