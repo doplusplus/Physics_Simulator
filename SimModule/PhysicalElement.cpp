14132385acad695755439a205b73f7c0fa9c9a06
Doz <doraneplus@gmail.com>
2015-10-11 14:49:37 +0100

    Update PhysicalElement.cpp

--- SimModule/PhysicalElement.cpp
+++ SimModule/PhysicalElement.cpp
@@ -1,147 +1,90 @@
-#include "PhysicalElement.h"
-#include <iostream>
+#ifndef PhysElem
+#define PhysElem
 
-//-------------------------- Instanciated Vector ------------------------------------
+#include "Geo.h"
 
-//Constructors and destructor
-InstanciatedVect::InstanciatedVect()
-{}
 
-InstanciatedVect::InstanciatedVect(Vect V)
+
+//****************************************************************************
+
+class InstanciatedVect
 {
-	Point P;
-	P.nullify();
-	application = &P; 
-	direction = V;
-}
+private:
+	Point * application;
+	Vect  direction;
 
-InstanciatedVect::InstanciatedVect(Point P, Vect V)
-{
-	application = &P;
-	direction = V;
-}
-
-InstanciatedVect::~InstanciatedVect()
-{}
-
-//Accessors
-Vect InstanciatedVect::getDirection() { return direction; } //**
-
-Point *InstanciatedVect::getApplicationPoint() //returns a pointer to the application point**
-{
-	return application;
-}
-
-//Display
-void InstanciatedVect::show()
-{
-	std::cout << "Direction:" << '\n';
-	direction.show();
-
-	std::cout << "Application point:" << '\n';
-	application->show();
-}
-
-//Modifier
-void InstanciatedVect::setDirection(Vect V)     //**
-{ direction.set(V); }
-
-void InstanciatedVect::setDirection(double x, double y, double z)
-{ direction.set(x,y,z); }
-
-
-void InstanciatedVect::setApplication(Point *P)
-{ application = P; }
-
-void InstanciatedVect::nullify()
-{
-	direction.nullify();
-	application->nullify();
-}
-
-
-
-
-//-------------------------- Forces ------------------------------------
-
+public:
 	//Constructors and destructor
-	Force::Force()
-	{}
+	InstanciatedVect();
+	InstanciatedVect(const InstanciatedVect &Iv);
+	InstanciatedVect(Vect V);
+	InstanciatedVect(Point &P, Vect V);
+	~InstanciatedVect();
 
-	Force::Force(Point P, Vect V) : InstanciatedVect(P,V)
-	{}
+	//Accessors
+	Vect getDirection(); //**
+	Point *getApplicationPoint(); //returns a pointer to the application point**
 
-	Force::~Force()
-	{}
-		
 	//Display
-	void Force::show()
-	{
-		std::cout << "   ---- Force ----   " << '\n';
-		InstanciatedVect::show();
-	}
-	
-	
-    Force Force::operator +(Force B ) //returns the sum of 2 forces applying to their middlepoint
-    {
-	 Force F;
-        F.setDirection(getDirection() + B.getDirection());
-		F.getApplicationPoint()->set(*getApplicationPoint() + *(B.getApplicationPoint()));
-     return F;
-    }
+	void show();
 
-    Force Force::operator -()
-    {
-	 Force F; Vect V;
-        double x=this->getDirection().getX();
-        double y=this->getDirection().getY();
-        double z=this->getDirection().getZ();
+	//Modifier
+	void setDirection(Vect V);     //**
+	void setDirection(double x, double y, double z);
+	void setApplication(Point *P);
+	void nullify();
+};
 
-        x=-x;y=-y;z=-z;
+//******************************************************************
 
-        V.set(x,y,z);
-        F.setDirection(V);
-        F.setApplication( getApplicationPoint());
-     return F;
-    }
+class Force : public InstanciatedVect    	//in Newton
+{
+public:
+	//Constructors and destructor
+	Force();
+	Force(Point &P, Vect V);
+	~Force();
 
-
-
-//-------------------------- Acceleration ------------------------------------
-//Constructors and destructor
-	Acceleration::Acceleration()
-	{}
-	
-	Acceleration::Acceleration(Force Resultant, double mass) 
-	{// returns the linear acceleration of the center of gravity of an object given its mass and resultant
-	
-        application = Resultant.getApplicationPoint();
-		direction = Resultant.getDirection() / mass;
-	}
-	
-	Acceleration::~Acceleration()
-	{}
-	
 	//Display
-	void Acceleration::show()
-	{
-		std::cout << "   ---- Acceleration ----   " << '\n';
-		InstanciatedVect::show();
-	}
-	
+	void show();
 
+	//Modifier	
+	void operator +(Force &B);	 	//adds up the right end of the + to the left one; placing the force in the middle
+	Force operator -();			//returns opposite force
 
-//-------------------------- Velocity ------------------------------------
-//Constructors and destructor
-	Velocity::Velocity()
-	{}
-	
-	Velocity::~Velocity()
-	{}
-	
+};
+
+class Acceleration : public InstanciatedVect                      // m/s²
+{
+public:
+	//Constructors and destructor
+	Acceleration();
+	Acceleration(Point &P, Vect V);
+	Acceleration(Force &Resultant, double mass);				// valid for point element
+	~Acceleration();
+
 	//Display
-	void Velocity::show()
-	{
-		std::cout << "   ---- Velocity ----   " << '\n';
-		InstanciatedVect::show();
-	}
+	void show();
+};
+
+class Velocity : public InstanciatedVect                     // m/s
+{
+public:
+	//Constructors and destructor
+	Velocity();
+	Velocity(Point &P, Vect V);
+	~Velocity();
+
+	//Display
+	void show();
+};
+
+struct timedForce  //all public
+{
+	double startTime;
+	double endTime;
+	Force F;
+};
+
+
+#endif
