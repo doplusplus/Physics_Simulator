b1fe866ee07c3945131812e403678030c541f15c
Doz <doraneplus@gmail.com>
2016-01-11 14:42:36 +0000

    Update Geo.cpp

--- SimModule/Geo.cpp
+++ SimModule/Geo.cpp
@@ -3,184 +3,206 @@
 #include <cmath>
 
 //----------------------------- Cartesian element ------------------------------------
-
-// Constructors and destructor
 CartesianElement::CartesianElement() { X = Y = Z = 0; }
 
-CartesianElement::CartesianElement(double x, double y, double z) 
+CartesianElement::CartesianElement(double x, double y, double z)
 {
 	X = x;
 	Y = y;
 	Z = z;
 }
 
-CartesianElement::~CartesianElement(){}
+CartesianElement::~CartesianElement() {}
 
-// Accessors
-double CartesianElement::getX() { return X; } //**  double
-double CartesianElement::getY() { return Y; } //**
-double CartesianElement::getZ() { return Z; } //**
+//Accessors
+double CartesianElement::xComponent() { return X; }
+double CartesianElement::yComponent() { return Y; }
+double CartesianElement::zComponent() { return Z; }
 
-// Display
+//Displays
 void CartesianElement::show()
 {
 	std::cout << "x:" << X << '\n' << "y:" << Y << '\n' << "z:" << Z << '\n';
 }
 
-// Modifier
-void CartesianElement::set(double x, double y, double z) // place the point a the coordinates (x,y,z)  **
+//Modifiers
+void CartesianElement::place(double x, double y, double z)
 {
 	X = x;
 	Y = y;
 	Z = z;
 }
 
-void CartesianElement::set(CartesianElement C)
+void CartesianElement::place(CartesianElement C)
 {
 	X = C.X;
 	Y = C.Y;
 	Z = C.Z;
 }
 
-void CartesianElement::nullify() 
+void CartesianElement::nullify()
 {
 	X = Y = Z = 0;
 }
 
-// Algebraic operators
-CartesianElement CartesianElement ::operator +(CartesianElement B)
+//Algebraic operators
+CartesianElement CartesianElement ::operator +(const CartesianElement &B)
 {
 	CartesianElement C(X + B.X, Y + B.Y, Z + B.Z);
 	return C;
 }
 
-CartesianElement CartesianElement ::operator -()		// returns the opposite element
+CartesianElement CartesianElement ::operator -()
 {
- CartesianElement C;
+	CartesianElement C;
 	C.X = -X;
 	C.Y = -Y;
 	C.Z = -Z;
- return C;
+	return C;
 }
 
-CartesianElement CartesianElement ::operator *(double a)	// multiplication by a scalar
+CartesianElement CartesianElement ::operator *(double a)	//multiplication by a scalar
 {
 	CartesianElement C(X *a, Y *a, Z *a);
 	return C;
 }
 
-CartesianElement CartesianElement ::operator /(double a)	// division by a scalar **
+CartesianElement CartesianElement ::operator /(double a)
 {
- CartesianElement C;
+	CartesianElement C;
 	if (a == 0)
 	{
 		C.nullify();
 		throw "divideByZero";
 	}
-	C.set(X / a, Y / a, Z / a);
- return C;
-}				
+	C.place(X / a, Y / a, Z / a);
+	return C;
+}
 
-//Logical operator	
-bool CartesianElement ::operator ==(CartesianElement B) 
+//logical operator	
+bool CartesianElement ::operator ==(CartesianElement B)
 {
 	return (X == B.X) && (Y == B.Y) && (Z == B.Z);
 }
-//--------------------------------------------------------------------------------------------------
-//==================================================================================================
 
 
 
+//----------------------------------------------------------------------------------------------------
+//--------------------------------POINT---------------------------------------------------------------
+Point::Point()
+{}
+
+Point::Point(double x, double y, double z) :CartesianElement(x, y, z)
+{}
+
+Point::Point(CartesianElement C)
+{
+	place(C);
+}
+
+Point::Point(Vect V)
+{
+	place(V);
+}
+
+Point::Point(Point P, Vect V)
+{
+	place(P + V);
+}
+
+Point::~Point()
+{}
 
 
+//--------------------------Memory tools ------------------------------------------
+std::vector< Point* > Point::OnHeap;
 
-//----------------------------------------------- POINT -----------------------------------------------
-// Constructors	and destructor
-	Point::Point()
-	{}
-	
-	Point::Point(double x,double y, double z):CartesianElement(x,y,z)
-	{/*that's pretty it*/}
+void Point::trackAsHeap()
+{
+	OnHeap.push_back(this);
+}
 
-	Point::Point(CartesianElement C)
+
+void Point::showHeapPoints()
+{
+	for (auto it = OnHeap.begin(); it != OnHeap.end(); it++)
 	{
-		set(C);
+		(**it).show();
 	}
+}
 
-	Point::Point(Vect V)  //creates a point at the end of the instance of V starting at the origin
-	{
-		set(V);
-	}
+double Point::PointsHeapSize()
+{
+	return OnHeap.size();
+}
 
-	Point::Point(Point P, Vect V) //creates a point at the end of the instance of V starting at point P
+void Point::clearHeapPoints()
+{
+	while (!OnHeap.empty())
 	{
-		set(P + V);
+		Point *cleanser = OnHeap.back();
+		OnHeap.pop_back();
+		delete cleanser;
+		cleanser = nullptr;
 	}
-	
-	Point::~Point()
-	{}
+}
 
 //.................................End Point......................................
 //================================================================================
 
 
 
+//================================================================================
 //----------------------------------- Vect -------------------------------------
-	
-// Constructors	and destructor
-	Vect::Vect()
-	{}
-	
-	Vect::Vect(double x, double y, double z) : CartesianElement(x, y, z)
-	{/*that's pretty it*/}
 
-	Vect::Vect(CartesianElement C)
-	{
-		set(C);
-	}
+//constructors	and destructor
+Vect::Vect()
+{}
 
-	Vect::Vect(Point P)
-	{
-		set(P);
-	}
+Vect::Vect(double x, double y, double z) :CartesianElement(x, y, z)
+{}
 
-	Vect::Vect(Point start, Point end)
-	{
-		set(end + (-start));
-	}
-	
-	Vect::~Vect()
-	{}
-	
-	Vect Vect::operator ^( Vect &B ) //produit Vectiel
-	{
-	 double x, y, z;
-		x = getY()*B.getZ()- getZ()*B.getY();
-		y = getZ()*B.getX() - getX()*B.getZ();
-		z = getX()*B.getY() - getY()*B.getX();
-		Vect v(x, y, z);
-	 return v;
-	}
-	
-	double Vect::operator *(Vect &B) //dot product
-    	{
-         double s;
-        	s= getX()*B.getX() + getY()*B.getY() + getZ()*B.getZ();
-         return s;
-    	}
+Vect::Vect(CartesianElement C)
+{
+	place(C);
+}
 
 
-// toolbox
-	double Vect::norm()
-	{
+Vect::~Vect()
+{}
+
+//Operators
+Vect Vect::operator ^(Vect &B)
+{
+	double x, y, z;
+	x = yComponent()*B.zComponent() - zComponent()*B.yComponent();
+	y = zComponent()*B.xComponent() - xComponent()*B.zComponent();
+	z = xComponent()*B.yComponent() - yComponent()*B.xComponent();
+	Vect v(x, y, z);
+
+	return v;
+}
+
+double Vect::operator *(Vect &B)
+{
+	double s;
+	s = xComponent()*B.xComponent() + yComponent()*B.yComponent() + zComponent()*B.zComponent();
+	return s;
+}
+
+
+// Characteristic elements
+double Vect::norm()
+{
 	double sq = *this * (*this);
-		sq = sqrt(sq);
-	 return sq;
-	}
-	
-	Vect Vect::unitarized()
-    	{
-         Vect u;
-		u = (*this) / norm();
-         return u;
-    	}
+	sq = sqrt(sq);
+	return sq;
+}
+
+Vect Vect::unitVector()
+{
+	Vect u;
+	u = (*this) / norm();
+
+	return u;
+}
