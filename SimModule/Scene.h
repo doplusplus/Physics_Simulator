e48c71eb38ed24e26b93848313b125fd8e605f49
Doz <doraneplus@gmail.com>
2016-01-18 18:26:06 +0000

    Update Scene.h

--- SimModule/Scene.h
+++ SimModule/Scene.h
@@ -3,20 +3,18 @@
 
 #include <vector>
 #include "MaterialElement.h"
-#include "PhysicalElement.h"
+
 
 class Scene
 {
-	private:
-		std::vector<MaterialElement*> S;
-		double Time = 0;
 
-	public:
+
+public:
 
 	//Constructors and destructor
 	Scene();
 	~Scene();
-	
+
 	//Accessors
 	MaterialElement* getElement(unsigned int i);
 	double getTime();
@@ -25,15 +23,19 @@ class Scene
 	void consoleShow();
 
 	//Modifier
-    	void setForce(Force F, unsigned int place);		// adds force to element i starting from 0
-    	void update(double dt);				// calculates the scene configuration in dt seconds
+	void setForce(Vect F, unsigned int place);		// adds force to element i starting from 0
+	void update(double dt);				// calculates the scene configuration in dt seconds
 	void simulate(double step, double duration);	// simulates the scene of certain duration and increment
-	
-	
+
+
 	//----------Model interface-----------------------
 	void addMatPoint();
-    	void addMatPoint(double x, double y, double z, double mass = 0, double charge = 0);
-	
+	void addMatPoint(double x, double y, double z, double mass = 0, double charge = 0);
+
+private:
+	std::vector<MaterialElement*> S;
+	double Time = 0;
+
 };
 
 
