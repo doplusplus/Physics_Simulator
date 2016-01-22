7dc0a69a0718e372d2451916f7e383a5c525d07d
Doz <doraneplus@gmail.com>
2016-01-22 16:59:42 +0000

    obvious mutators removed

--- SimModule/Geo.h
+++ SimModule/Geo.h
@@ -3,65 +3,46 @@
 
 #include <vector>
 
-//------------------------------ CartesianElement-------------------------------------------------
-
+//==================================== CartesianElement ====================================
 class CartesianElement
 {
 public:
 	CartesianElement();
 	CartesianElement(double x, double y, double z);
-	~CartesianElement();
+	CartesianElement(const CartesianElement &C);
+	virtual ~CartesianElement();
 
 	//accessor
-	double xComponent();
-	double yComponent();
-	double zComponent();
+	double xComponent() { return X; };
+	double yComponent() { return Y; };
+	double zComponent() { return Z; };
 
 	//Display
 	void show();
-
+	
 	//Modifier
-	void place(double x, double y, double z);
-	void place(CartesianElement C);				// places on C
+	void move(double dx, double dy, double dz);
 	void nullify();
 
 	//Algebraic operator
 	CartesianElement operator +(const CartesianElement &B);
 	CartesianElement operator-();
+	CartesianElement operator -(CartesianElement B);
+
 	CartesianElement operator *(double a);			// multiplication by a scalar
 	CartesianElement operator /(double a);			// division by a scalar, exception thrown if null 
 
 //logical operator	
 	bool operator ==(CartesianElement B);
 
-private:
+protected:
 	double X;
 	double Y;
 	double Z;
 };
 
-
-
-//--------------------------------- Vect --------------------------------------------------
-class Vect : public CartesianElement
-{
-public:
-	Vect();
-	Vect(double x, double y, double z);
-	Vect(CartesianElement C);
-	~Vect();
-
-	Vect operator ^(Vect &B);		// Vectorial multiplication
-	double operator *(Vect &B);		// dot product	
-	Vect unitVector();
-	double norm();
-
-};
-
-
-
-//--------------------------------- Point --------------------------------------------------
-
+//==================================== Point ====================================
+class Vect;
 class Point : public CartesianElement
 {
 public:
@@ -69,11 +50,25 @@ public:
 	Point(double x, double y, double z);
 	Point(CartesianElement C);
 	Point(Vect V);				//At the non null extremity of V 
-	Point(Point P, Vect V);			//At the image of P translated by V 
+	Point(Point P, Vect V);		//At the image of P translated by V 
 	~Point();
-
 };
-//-----------------------------------------------------------------------------------------
 
+//==================================== Vect ====================================
+
+class Vect : public CartesianElement
+{
+public:
+	Vect();
+	Vect(double x, double y, double z);
+	Vect(CartesianElement C);
+	Vect(Point A, Point B);
+	~Vect();
+
+	Vect operator ^(Vect &B);		// Vectorial multiplication
+	double operator *(Vect &B);		// dot product	
+	Vect unitVector();
+	double norm();
+};
 
 #endif
